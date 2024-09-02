c578b1a5a73cb81bb09e58d8c799c9955a0d98c7
Rustam8217 <gadzhiev.r.s@gmail.com>
2024-09-02 23:04:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PerformanceLabTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.GetCirculArarrayPath(19,3);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ task1/Task1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceLabTask
+{
+    public class Task1
+    {
+        private const int CLOSING_VALUE = 1;
+
+        public static void GetCirculArarrayPath(int n, int m)
+        {
+            Console.WriteLine(GetCurcularValue(n));
+        }
+
+        private static List<int> GetCurcularArrays(int initialValue, int subarrayLength)
+        {
+            List<int> ints = new List<int>();
+            int currentValue = initialValue;
+
+            do
+            {
+                ints.Add(currentValue);
+
+
+            } while (currentValue % 10 != CLOSING_VALUE);
+        }
+
+
+        private static int ShiftDigits(int firstDigit, int initialValue)
+        {
+            int result = initialValue;
+
+            while (GetFirstDigit(result) != firstDigit)
+            {
+
+            }
+        }
+
+        private static int GetFirstDigit(int value)
+        {
+            while (value / 10 > 0) value /= 10;
+            return value;
+        }
+
+        private static int GetCurcularValue(int initalValue)
+        {
+            int result = 0;
+
+            for(int i = 0; i <= initalValue; i++)
+            {
+                result *= 10;
+                result += i;
+            }
+
+            return result;
+        }
+    }
+}
